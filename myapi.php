5c1c096dbeb25178292e6873abd653ca59b7f773
HossainNur <nurhossainsani1@gmail.com>
2024-06-14 22:01:39 +0600

    request method check

--- myapi.php
+++ myapi.php
@@ -1,3 +1,28 @@
 <?php
-echo "My first PHP api testing";
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+    echo '{"result": "get received"}';
+    break;
+
+    case 'POST':
+    echo '{"result": "post received"}';
+    break;
+
+    case 'PUT':
+    echo '{"result": "put received"}';
+    break;
+
+    case 'DELETE':
+    echo '{"result": "delete received"}';
+    break;
+
+    default:
+    echo '{"result": "un defiend"}';
+    break;
+}
+
 ?>
